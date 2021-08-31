b7364693179376504d39a19ed45f8c12125d1c79
Marta Skibicka <marta.skibicka.contact@gmail.com>
2021-08-31 21:27:30 +0200

    Aktualizacja plików

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, redirect
 import tkinter as tk
 from tkinter.messagebox import showinfo
+from main import Finding
 
 app = Flask(__name__)
+finder = Finding
 
 @app.route("/")
 def main():
@@ -15,4 +17,9 @@ def about():
 @app.route("/attach-file-instruction/")
 def attach():
     return render_template("attach.html")
+
+@app.route("/finding-play-button/")
+def play():
+
+    return render_template("play.html")
 
--- main.py
+++ main.py
@@ -12,15 +12,20 @@ import sys
 import pyautogui
 import cv2
 import numpy as np
+import tkinter as tk
 
-play_button = "play_button.png"
-chrome_button = "chrome_button.png"
+play_button = "images/play_button.png"
+chrome_button = "images/chrome_button.png"
+write_button = "images/write_button.png"
+to_who = "images/to_who.png"
+subject = "images/subject.png"
+send_button = "images/send.png"
+attach = "images/attach_files.png"
 
 pyautogui.screenshot("ss.png")
 ss = "ss.png"
 
-akcja = sys.argv[1]
-
+action = sys.argv[1]
 
 class Finding:
 
@@ -28,19 +33,67 @@ class Finding:
         img_rgb = cv2.imread(ss)
         template = cv2.imread(find)
         w, h = template.shape[:-1]
+        print(template.shape)
         result = cv2.matchTemplate(img_rgb, template, cv2.TM_CCOEFF_NORMED)
+
+
         threshold = .8
         loc = np.where(result >= threshold)
+        print(loc)
+    
         for pt in zip(*loc[::-1]):
-            cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
-        cv2.imwrite('result.png', img_rgb)
+            cv2.rectangle(img_rgb, pt, (pt[0] + h, pt[1] + w), (0, 0, 255), 2)
 
 
-if akcja == "play":
+        ''' <--- działa na "write_button" -->
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+        top_left = (min_loc[0] + h, min_loc[1] + w)
+        print(top_left)
+        print(w)
+        print(h)
+        print(min_loc)
+        print(max_val)
+
+        bottom_right = (top_left[0] + h, top_left[1] + w)
+
+        print(top_left)
+        print(bottom_right)
+
+        cv2.rectangle(img_rgb,top_left, bottom_right, (0,0,255), 2)
+        <--- działa na "write_button" --> '''
+
+        cv2.imwrite("result.png", img_rgb)
+
+
+        #cv2.imshow('result.png', img_rgb)
+        #cv2.waitKey(0)
+
+
+if action == "play":
     find = play_button
     Finding.show_find_thing(find)
 
-if akcja == "chrome":
+
+if action == "chrome":
     find = chrome_button
     Finding.show_find_thing(find)
 
+if action == "write":
+    find = write_button
+    Finding.show_find_thing(find)
+
+if action == "send":
+    find = send_button
+    Finding.show_find_thing(find)
+
+if action == "files":
+    find = attach
+    Finding.show_find_thing(find)
+
+if action == "who":
+    find = to_who
+    Finding.show_find_thing(find)
+
+if action == "subject":
+    find = subject
+    Finding.show_find_thing(find)
